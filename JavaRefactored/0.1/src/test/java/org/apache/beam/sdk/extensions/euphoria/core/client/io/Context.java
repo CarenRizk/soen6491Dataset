9b9c0a08151b2332ffa85fa24f5e3cc8f263d7f3
Caren Rizk <carenrizk@microsoft.com>
2024-11-10 10:52:14 -0500

    Removed files that are no needed

--- JavaRefactored/0.1/src/test/java/org/apache/beam/sdk/extensions/euphoria/core/client/io/Context.java
+++
@@ -1,7 +0,0 @@
-package org.apache.beam.sdk.extensions.euphoria.core.client.io;
-
-import org.apache.beam.sdk.extensions.euphoria.core.annotation.audience.Audience;
-
-// Optimized by LLM: Removed @Deprecated annotation as the interface is still in use
-@Audience(Audience.Type.CLIENT)
-public interface Context extends Environment {}
--- JavaRefactored/main/java/org/apache/beam/runners/dataflow/IdentityMessageFn.java
+++
@@ -1,7 +0,0 @@
-package org.apache.beam.runners.dataflow;
-
-import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
-import org.apache.beam.sdk.transforms.SimpleFunction;
-
-// Optimized by LLM: Removed IdentityMessageFn class and used a lambda expression
-SimpleFunction<PubsubMessage, PubsubMessage> identityMessageFn = input -> input;
